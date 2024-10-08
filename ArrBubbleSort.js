226e79d8223e85915b1417b10ae6c7639d2941bf
YuraFedorets <52930288+YuraFedorets@users.noreply.github.com>
2024-10-08 19:50:15 +0300

    Add files via upload

---
+++ ArrBubbleSort.js
@@ -0,0 +1,27 @@
+let arr = [];
+let kind;
+function ArrSort(arr,kind){
+    if(kind===true){
+        for(let i = 0; i < arr.length;i++){
+            for(let j = 0; j < arr.length-1; j++){
+                if(arr[j] > arr[j+1]){
+                    const temp = arr[j];
+                    arr[j] = arr[j+1];
+                    arr[j+1] = temp
+                }
+            }
+        }
+    }else{
+        for(let i=0; i<arr.length;i++){
+            for(let j = 0; j < arr.length-1; j++){
+                if(arr[j] < arr[j+1]){
+                    const temp = arr[j];
+                    arr[j] = arr[j+1];
+                    arr[j+1] = temp
+                }
+            }
+        }
+    }
+return arr;
+}
+console.log(ArrSort([3,5,9,-1],true)); 
